16b42eac3f115dfac7fee5094cf60996318c9baa
Jadrdc <jadrdc@gmail.com>
2018-12-18 23:14:50 -0400

    ButterKnife added to the project

--- app/src/main/java/agustinreinosowerewolf/com/werewolfgamemoderator/SignActivity.java
+++ app/src/main/java/agustinreinosowerewolf/com/werewolfgamemoderator/SignActivity.java
@@ -2,12 +2,28 @@ package agustinreinosowerewolf.com.werewolfgamemoderator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.common.SignInButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class SignActivity extends AppCompatActivity {
+
+    @BindView(R.id.sign_in_button)
+    public SignInButton mSignButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_activity);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.sign_in_button)
+    public void SignIn(View view) {
+
     }
 }
